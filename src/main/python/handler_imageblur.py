10913b5105f46a6659be40a8fbd671095ea29707
Saurav Kumar <2020saurav@gmail.com>
2016-04-17 16:32:23 +0530

    Added text in imageblur docker image

--- src/main/python/handler_imageblur.py
+++ src/main/python/handler_imageblur.py
@@ -3,17 +3,20 @@
 # The module must be named handler.py and kept in /root/chariot/ of the docker container
 # Use base container 2020saurav:chariot from docker hub to build applications.
 
-from PIL import ImageFilter, Image
+from PIL import ImageFilter, Image, ImageFont, ImageDraw
 import io
 import base64
 
 def handle(request):
-    return globals()[request['function_name']](request)
+  return globals()[request['function_name']](request)
 
 def blur(request):
-    imgBytes = base64.b64decode(request['extra_data']['imgBytes'])
-    image    = Image.open(io.BytesIO(imgBytes))
-    blurImg  = image.filter(ImageFilter.BLUR)
-    blurImg.save('/tmp/'+request['request_id']+'.jpg')
-    blurStr = base64.b64encode(open('/tmp/'+request['request_id']+'.jpg', 'rb').read())
-    return {'blurStr': blurStr}
+  imgBytes = base64.b64decode(request['extra_data']['imgBytes'])
+  image    = Image.open(io.BytesIO(imgBytes))
+  blurImg  = image.filter(ImageFilter.BLUR)
+  draw     = ImageDraw.Draw(blurImg)
+  font     = ImageFont.truetype("/root/chariot/sans.ttf", 20)
+  draw.text((0, 0), "CHARIOT", (255, 40, 40), font=font) 
+  blurImg.save('/tmp/'+request['request_id']+'.jpg')
+  blurStr = base64.b64encode(open('/tmp/'+request['request_id']+'.jpg', 'rb').read()).decode('utf-8')
+  return {'blurStr': blurStr}
